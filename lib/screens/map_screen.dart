41192a7e6283c9058b0949e9dc12fb0c4dbad1bb
arthursaocin <arthursaocin@gmail.com>
2025-01-11 21:19:51 +0700

    update last

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,42 +1,126 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:geolocator/geolocator.dart';
 import '../models/restaurant.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
   final Restaurant restaurant;
 
   const MapScreen({super.key, required this.restaurant});
 
   @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  Position? _currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation(); // Mendapatkan lokasi saat ini
+  }
+
+  Future<void> _getCurrentLocation() async {
+    // Meminta izin lokasi
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Tidak ada layanan lokasi
+      return Future.error('Layanan lokasi tidak aktif.');
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Izin ditolak
+        return Future.error('Izin lokasi ditolak.');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Izin ditolak secara permanen
+      return Future.error(
+          'Izin lokasi ditolak secara permanen, tidak dapat meminta izin.');
+    }
+
+    // Mendapatkan posisi saat ini
+    final position = await Geolocator.getCurrentPosition();
+    setState(() {
+      _currentPosition = position;
+    });
+  }
+
+  void _launchMaps(double destinationLat, double destinationLng) async {
+    if (_currentPosition != null) {
+      final url =
+          'https://www.google.com/maps/dir/?api=1&origin=${_currentPosition!.latitude},${_currentPosition!.longitude}&destination=$destinationLat,$destinationLng';
+      if (await canLaunchUrl(Uri.parse(url))) {
+        await launchUrl(Uri.parse(url), mode: LaunchMode.externalApplication);
+      } else {
+        throw 'Could not launch $url';
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Lokasi saat ini tidak tersedia')),
+      );
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Lokasi ${restaurant.name}'),
+        title: Text('Lokasi ${widget.restaurant.name}'),
       ),
-      body: FlutterMap(
-        options: MapOptions(
-          initialCenter: LatLng(restaurant.latitude, restaurant.longitude),
-          initialZoom: 16.0, // Atur tingkat zoom
-        ),
+      body: Stack(
         children: [
-          TileLayer(
-            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-            subdomains: ['a', 'b', 'c'],
-          ),
-          MarkerLayer(
-            markers: [
-              Marker(
-                width: 80.0,
-                height: 80.0,
-                point: LatLng(restaurant.latitude, restaurant.longitude),
-                child: const Icon(
-                  Icons.location_on,
-                  color: Colors.red,
-                  size: 40.0,
-                ),
+          FlutterMap(
+            options: MapOptions(
+              initialCenter: LatLng(widget.restaurant.latitude,
+                  widget.restaurant.longitude),
+              initialZoom: 16.0,
+            ),
+            children: [
+              TileLayer(
+                urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                subdomains: ['a', 'b', 'c'],
+              ),
+              MarkerLayer(
+                markers: [
+                  Marker(
+                    width: 80.0,
+                    height: 80.0,
+                    point: LatLng(widget.restaurant.latitude,
+                        widget.restaurant.longitude),
+                    child: const Icon(
+                      Icons.location_on,
+                      color: Colors.red,
+                      size: 40.0,
+                    ),
+                  ),
+                ],
               ),
             ],
+          ),
+          Positioned(
+            bottom: 20,
+            left: 20,
+            right: 20,
+            child: ElevatedButton.icon(
+              onPressed: () {
+                _launchMaps(widget.restaurant.latitude,
+                    widget.restaurant.longitude);
+              },
+              icon: const Icon(Icons.navigation),
+              label: const Text('Panduan Arah'),
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                backgroundColor: Colors.blue,
+              ),
+            ),
           ),
         ],
       ),
